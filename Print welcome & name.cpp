ac2b3c0a0e0740b7ad7958589430bc773937fca4
Ahmad sindi <ahmadaalsindi@gmail.com>
2025-10-19 19:15:32 +0300

    Create Print welcome & name.cpp

---
+++ Print welcome & name.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+   std::string names;
+   std::cout << "What's your name \n";
+   std::cin >> names;
+   std::cout << "Welcome to our  ... : " << names;
+    return 0;
+}
